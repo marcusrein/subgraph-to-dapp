7ba0661e15f44a7fb79b8626c5cac24dead8c8b8
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2023-01-08 06:55:39 -0500

    successfully added gql client and integrated w dapp

--- app.py
+++ app.py
@@ -1,34 +1,13 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 from json2html import *
-import requests
-import json
+from gql import Client, gql
+from gql.transport.aiohttp import AIOHTTPTransport
 import os
 
 app = Flask(__name__)
 if __name__ == '__main__':
     app.run()
-
-def update_query(input_number): 
-    query = """
-        {
-        swaps(orderBy: amountInUSD, orderDirection: desc, first: replaceMe) {
-            amountInUSD
-            from
-            to
-            timestamp
-            tokenIn {
-            name
-            }
-            tokenOut {
-            name
-            }
-        }
-        }
-    """.replace('replaceMe', input_number)
-    return query
-
-# subgraph_endpoint = 'https://gateway.thegraph.com/api/[api-key]/subgraphs/id/ELUcwgpm14LKPLrBRuVvPvNKHQ9HvwmtKgKSH6123cr7'
 
 # Environment Variable Management
 load_dotenv()
@@ -36,19 +15,45 @@ api_key = os.getenv('SUBGRAPH_API')
 subgraph_endpoint = f'https://gateway.thegraph.com/api/{api_key}/subgraphs/id/ELUcwgpm14LKPLrBRuVvPvNKHQ9HvwmtKgKSH6123cr7'
 
 
+transport = AIOHTTPTransport(
+    url=subgraph_endpoint
+)
+
+client = Client(
+    transport=transport,
+    fetch_schema_from_transport=True,
+)
+
+def update_subgraph_query(input_number):
+    subgraph_gql_query = gql(
+                """
+                {
+                swaps(orderBy: amountInUSD, orderDirection: desc, first: replaceMe) {
+                    amountInUSD
+                    from
+                    to
+                    timestamp
+                    tokenIn {
+                    name
+                    }
+                    tokenOut {
+                    name
+                    }
+                }
+                }
+            """.replace('replaceMe', input_number))
+    return subgraph_gql_query
+
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        swapInputNumberData = request.form["top_swap_count"]
-        query = update_query(swapInputNumberData)
-        response = requests.post(url=subgraph_endpoint, json={"query": query})
-        print(response)
-        formatted_subgraph_json = json.loads(response.content)
-        json_table = json2html.convert(json = formatted_subgraph_json)
-       
-        if response.status_code == 200:
-            return render_template('index.html', json_table = json_table)
-
+        swap_input_number_data = request.form["top_swap_count"]
+        subgraph_gql_query_result = update_subgraph_query(swap_input_number_data)
+        response = client.execute(subgraph_gql_query_result)
+        json_table = json2html.convert(json = response)
+        return render_template('index.html', json_table = json_table)
 
     else:
         return render_template('index.html')
